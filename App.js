f42214d1e0ae7879ff378f1b636172bf32f25489
Vinicius Santos <santos.viniciusmt@gmail.com>
2020-09-10 16:39:16 -0300

    begin of work

--- App.js
+++ App.js
@@ -1,14 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+
+import CountDown from 'react-native-countdown-component';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  const [number, setNumber] = useState()
+
+//   const timer = props => {
+//   const [seg, setSeg] = useState(0);
+
+//   setInterval(() => {
+//     if (seg == 0) alert("Tempo Acabou");
+//     if (seg > 0) setSeg(seg - 1);
+//   }, 1000);
+
+//   return (
+//     <TextInput
+//       value={seg.toString()}
+//       onChangeText={text => setSeg(Number.parteInt(text))}
+//       keyboardType='numeric'
+//     />
+//   )
+// }
+
+function handleAdd(){
+  
+  var numberStr = number.toString();
+
+  if(number == 0){
+    alert("time 0");
+  } else{
+    return(
+      <CountDown
+      until={number}
+      onFinish={() => alert(number)}
+      onPress={() => alert('hello')}
+      size={20}
+    />
+    )
+  }
+
+
+}
+
+return (
+  <View style={styles.container}>
+
+    <CountDown
+      until={number}
+      onFinish={() => alert()}
+      onPress={() => alert('hello')}
+      size={20}
+    />
+
+    <TextInput style={styles.textInput}
+      
+      placeholder="Type the seconds"
+      placeholderTextColor="#000"
+      
+      onChangeText={(value) => setNumber(value)}>
+    </TextInput>
+
+    <TouchableOpacity style={styles.button} onPress={handleAdd}>
+      <Text style={styles.buttonText}>Press me</Text>
+    </TouchableOpacity>
+
+    <TouchableOpacity style={styles.button}>
+      <Text style={styles.buttonText}>Press me2</Text>
+    </TouchableOpacity>
+  </View>
+)
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +80,28 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#FAB913',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 15,
+    borderRadius: 10
+  },
+  buttonText: {
+    fontSize: 17,
+    fontWeight: 'bold'
+  },
+  textInput: {
+    marginTop: 15,
+    backgroundColor: '#FAB913',
+    width: 200,
+    height: 50,
+    borderRadius: 10,
+    textAlign: 'center',
+  },
+  textInputText: {
+    color: '#000'
+  }
 });
